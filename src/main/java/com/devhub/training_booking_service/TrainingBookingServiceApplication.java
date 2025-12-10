e144966a90b72fd536e89f4f2a655a5b6ab2d40c
AnishJaiswal4444 <anishjaiswal381@gmail.com>
2025-12-11 02:30:14 +0530

    initial sring boot project setup

---
+++ src/main/java/com/devhub/training_booking_service/TrainingBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devhub.training_booking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingBookingServiceApplication.class, args);
+	}
+
+}
